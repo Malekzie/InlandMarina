cd65f1ee2ba4ed4b9bb3d997a4dd9b931b94ded4
Robbie Soriano <20562606-grapeberryfruit@users.noreply.gitlab.com>
2024-07-16 15:48:51 -0600

    Add project files.

---
+++ Marina/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Marina.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
