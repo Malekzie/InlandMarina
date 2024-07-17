a396aa4941f21e1af70ed2c77caf0707d68b3b2f
Robbie Soriano <grapeberryfruit@gmail.com>
2024-07-17 15:05:49 -0600

    View leasing

---
+++ Marina/Areas/Customer/Controllers/LeaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Marina.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize(Roles = "Customer")]
+    public class LeaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
