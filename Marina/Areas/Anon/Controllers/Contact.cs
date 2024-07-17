e56bb43e830bc26af9daa13f971f91f9921697e7
Robbie Soriano <grapeberryfruit@gmail.com>
2024-07-17 14:33:49 -0600

    Added Contacts

---
+++ Marina/Areas/Anon/Controllers/Contact.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Marina.Areas.Anon.Controllers
+{
+    [Area("Anon")]
+    public class Contact : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
